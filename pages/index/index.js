674a5f79998240ba538283ca720fd408d54aea1b
19045884 <19045884@suning.com>
2020-05-31 10:32:42 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,34 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    vHeight: 0,
+    vWidth: 0,
+    name: '陈杰',
+    spell: 'Chen Jie',
+    job: '前端开发工程师',
+    exp: '4年工作经验',
+    telphone: 15705180980,
+    sex: '男',
+    age: 30,
+    email: 'chenjiepds@163.com',
+    address: '江苏南京',
+    skills:[
+      {id: '1', name: 'javascript', percent: 85},
+      {id: '2', name: 'css', percent: 75},
+      {id: '3', name: 'html', percent: 85},
+      {id: '4', name: 'webpack', percent: 85},
+      {id: '5', name: 'vue', percent: 75},
+    ],
+    education: [
+      {id: '1', school: '南京理工大学', major: '电力电子与电力传动', year: '2013-2016'},
+      {id: '2', school: '平顶山学院', major: '电气工程及其自动化', year: '2009-2013'}
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  // 监听页面显示
+  onShow: function() {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      vHeight:　wx.getSystemInfoSync().windowHeight,
+      vWidth:　wx.getSystemInfoSync().windowWidth
     })
   }
 })
